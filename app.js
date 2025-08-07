df6fa95b90f46fe2dea6963d1728a89918f5ef00
bob-bober <nigel.ykaya@gmail.com>
2025-08-07 13:02:17 +0200

    added animationis
    hinzugefügt:
    -  scroll-animationen für feature-section
    - scroll-animationen für faq-section

--- app.js
+++ app.js
@@ -3,31 +3,11 @@
 window.Webflow = window.Webflow || [];
 window.Webflow.push(() => {
  
+alert("klappt es?")
 
-// scroll-animation feature-section
-gsap.utils.toArray(".feature-txt-wrapper").forEach((el) => {
 
-  gsap.from(el.children, {
-
-    scrollTrigger: {
-      trigger: el,
-      start: "top 85%",
-      markers: true,
-      toggleActions: "play none none none",
-    },
-
-    opacity: 0,
-    y: 50,
-    duration: 0.8,
-    stagger: 0.1,
-    ease: "power1.out",
-
-  });
-
-});
-
-// main-feature-scroll-animation
-
+/* main-feature-section */
+//scroll-animation-txt
 gsap.from(document.querySelector(".main-feature-content-wrapper").children, {
 
   scrollTrigger: {
@@ -45,8 +25,51 @@ gsap.from(document.querySelector(".main-feature-content-wrapper").children, {
 
 });
 
-imagesLoaded(document.querySelector("body"), () => {
-  ScrollTrigger.refresh();
+/* feature section */
+// scroll-animation-imgs
+gsap.utils.toArray(".feature-img").forEach((el) => {
+
+  const wrapper = el.closest(".feature-wrapper");
+
+  gsap.from(el, {
+    scrollTrigger: {
+      trigger: wrapper,
+      start: "top 65%",
+      markers: true,
+      toggleActions: "play none none reverse"
+    },
+
+    opacity: 0.3,
+    y: 50,
+    duration: 1,
+    ease: "power1.out"
+    
+  })
+});
+
+/* faq-section */
+// scroll-animation-borderlines
+gsap.from(".questions-wrapper .border-line", {
+  scrollTrigger: {
+    trigger: ".questions-wrapper",
+    start: "top 95%",
+    markers: true,
+    toggleActions: "play none none reset",
+  },
+
+  width: "0%",
+  stagger: 0.1,
+  duration: 2,
+  ease: "power1.out",
+
+})
+
+// hover-animation-bg
+
+
+/* scrollTrigger-refresh */
+window.addEventListener("load", () => {
+    scrollTrigger.refresh();
 });
 
 
@@ -56,3 +79,5 @@ imagesLoaded(document.querySelector("body"), () => {
 
 
 console.log("klappt das jetzt oder wie? ")
+
+console.log("test")
